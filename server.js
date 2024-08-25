6d7ec302ebf18fc94d17249040a7cf64d31f4850
himanshu-AW <himanshu.pro1810@gmail.com>
2024-08-25 13:18:12 +0530

    change port no

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 
 app.get("/", (req, res) => {

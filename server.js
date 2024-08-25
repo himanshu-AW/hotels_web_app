184ef7c9044b5c5436aa3091b4ea597cf09fe3e7
himanshu-AW <himanshu.pro1810@gmail.com>
2024-08-25 13:18:59 +0530

    change port no.

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.use('/menu', menuItemRotes);
 
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
